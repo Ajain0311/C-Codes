aff35701e483cceb8c45734e669d77ec20f549f3
Ajain0311 <adityajain8875389629@gmail.com>
2024-03-30 00:18:48 +0530

    easy tree implementations

---
+++ easy_tree.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+// Node definition for the binary tree
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// Function to perform inorder traversal of the binary tree
+void inorderTraversal(Node* root) {
+    if (root == nullptr) return;
+
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+int main() {
+    // Creating the binary tree
+    Node* root = new Node(5);
+    root->left = new Node(3);
+    root->right = new Node(8);
+    root->left->left = new Node(2);
+    root->left->right = new Node(4);
+    root->right->left = new Node(6);
+    root->right->right = new Node(9);
+
+    // Performing inorder traversal to print the elements
+    cout << "Inorder traversal (sorted order): ";
+    inorderTraversal(root);
+    cout << endl;
+
+    // Freeing memory
+    delete root->left->left;
+    delete root->left->right;
+    delete root->right->left;
+    delete root->right->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+
+    return 0;
+}
